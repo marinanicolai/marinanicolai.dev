3da54ff8629f3d7fa581c2210808337af3bd9881
marina <nicolaimarina@gmail.com>
2021-05-19 09:47:39 -0400

    update the certificate page

--- src/pages/cert.js
+++ src/pages/cert.js
@@ -1,8 +1,25 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
+import SEO from 'react-seo-component';
+import Layout from '~layouts/mainLayout';
 
+import { useSiteMetadata } from '~hooks/useSiteMetadata';
 
-const CertSection = () => (
-   <p> the certificates will go here </p>
-)
-
-export default CertSection;
+export default function Toolbox() {
+  const {
+    title,
+    image,
+    siteUrl,
+    siteLanguage,
+    siteLocale,
+    twitterUsername,
+  } = useSiteMetadata();
+  return (
+    <>
+      
+      <Layout>
+       
+      </Layout>
+    </>
+  );
+}
