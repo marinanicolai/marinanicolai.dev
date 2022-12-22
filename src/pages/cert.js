e31276268dbb0d973a30e012548ad540e59b41b8
marina <nicolaimarina@gmail.com>
2022-12-22 16:20:33 -0500

    adding the udemy certificates

--- src/pages/cert.js
+++ src/pages/cert.js
@@ -26,30 +26,35 @@ export default function Toolbox() {
             </a>
           </li>
 
-         
           <li>
-          <a
+            <a
               href="https://docs.google.com/document/d/18QZtiRl8lHZDSuREcUTTc2MWLFy85zvTshKpyTkvdQM/edit?usp=sharing"
               target="_blank"
               rel="noreferrer"
               className="text-base text-coolGray-600 dark:text-coolGray-400 hover:text-gray-900"
             >
-             NOVA Web Design and Development Certificate
+              NOVA Web Design and Development Certificate
             </a>
           </li>
           <li>
-            <h3>
-              {' '}
-              [Udemy: JavaScript:Understanding the weird
-              parts](link-to-the-photo){' '}
-            </h3>
+            <a
+              href="https://www.udemy.com/certificate/UC-41bafa2a-fc02-4afe-9a89-6045884ab69c/"
+              target="_blank"
+              rel="noreferrer"
+              className="text-base text-coolGray-600 dark:text-coolGray-400 hover:text-gray-900"
+            >
+              Getting started with React Hooks
+            </a>
           </li>
           <li>
-            <h3>
-              {' '}
-              [Udemy: JavaScript Algorithms and Data Structures
-              Masterclass](link-to-the-photo){' '}
-            </h3>
+            <a
+              href="https://www.udemy.com/certificate/UC-838393d9-f09a-4ab3-bd04-0916b13f3b81/"
+              target="_blank"
+              rel="noreferrer"
+              className="text-base text-coolGray-600 dark:text-coolGray-400 hover:text-gray-900"
+            >
+              Javascript Algorithms and Data Structures
+            </a>
           </li>
         </ul>
       </Layout>
