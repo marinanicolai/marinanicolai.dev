69578665a71930dcb8728d08ebb5842208fa5538
marina <nicolaimarina@gmail.com>
2022-12-28 06:48:49 -0500

    adding the second project

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,6 +3,7 @@ import React from 'react';
 import SEO from 'react-seo-component';
 import Layout from '~layouts/mainLayout';
 import FirstProjectImage from '~assets/first.png';
+import SecondProjectImage from '~assets/second.png';
 
 import { useSiteMetadata } from '~hooks/useSiteMetadata';
 
@@ -60,6 +61,46 @@ export default function Toolbox() {
                     className="select-none"
                     src={FirstProjectImage}
                     alt="firstprojectimg"
+                  />
+                </div>
+              </div>
+            </li>
+            <li className="bg-coolGray-100 dark:bg-blueGray-800 rounded-3xl">
+              <div className="p-6 md:p-10 flex flex-col-reverse ">
+                <div className="mt-8 md:max-w-lg">
+                  <p className="text-2xl lg:text-3xl font-extrabold text-coolGray-900 dark:text-white mb-2">
+                    project one
+                  </p>
+                  <p className="text-coolGray-600 dark:text-coolGray-400 prose leading-6 mb-3">
+                    <ul>
+                      <li>The project was created using Next.js and React</li>
+                      <li> Incorporated use of chakra-ui library</li>
+                    </ul>
+                  </p>
+                  <a
+                    href="https://real-estate-tau-lemon.vercel.app/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <div className="flex text-base sm:text-lg lg:text-base xl:text-lg font-medium transition-colors duration-200 focus:ring-2 focus:ring-offset-2 focus:ring-current focus:outline-none rounded-md text-orange-600 hover:text-orange-700 dark:text-orange-500 dark:hover:text-orange-400 mt-2">
+                      <p>Visit the live website</p>
+                    </div>
+                  </a>
+                  <a
+                    href="https://github.com/marinanicolai/real-estate-next.js"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <div className="inline-flex text-base sm:text-lg lg:text-base xl:text-lg font-medium transition-colors duration-200 focus:ring-2 focus:ring-offset-2 focus:ring-current focus:outline-none rounded-md text-orange-600 hover:text-orange-700 dark:text-orange-500 dark:hover:text-orange-400 mt-2">
+                      <p>Code source</p>
+                    </div>
+                  </a>
+                </div>
+                <div>
+                  <img
+                    className="select-none"
+                    src={SecondProjectImage}
+                    alt="secondprojectimg"
                   />
                 </div>
               </div>
