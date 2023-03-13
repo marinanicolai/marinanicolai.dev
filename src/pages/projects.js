004b6b39a8703e8a315a74aee7c49f1689bfa67d
marina <nicolaimarina@gmail.com>
2023-03-13 11:18:46 -0400

    edit the projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,6 +3,8 @@ import React from 'react';
 import SEO from 'react-seo-component';
 import Layout from '~layouts/mainLayout';
 import FirstProjectImage from '~assets/first.png';
+import TvImage from '~assets/tv-solid.svg';
+import GithubImage from '~assets/github.svg';
 import SecondProjectImage from '~assets/second.png';
 
 import { useSiteMetadata } from '~hooks/useSiteMetadata';
@@ -25,46 +27,58 @@ export default function Toolbox() {
                     Photo Gallery App
                   </p>
                   <p className="text-coolGray-600 dark:text-coolGray-400 prose leading-6 mb-3">
-                    <ul>
-                      <li>
-                        React application for filtering photos by name with
-                        design inspired by real sites
-                      </li>
-                      <li>Utilized public APIs to fetch data </li>
-                      <li>
-                        Gained experience in code architecture to separate
-                        functional logic from UI logic
-                      </li>
-                    </ul>
+                    This is a fun-based project that fetches data from The
+                    Unsplash Database API using React.js, allowing users to see
+                    images, and search for specific image within Unsplash's
+                    database. Users can select images and read additional
+                    information about them: author and how many likes it got;
+                    and saving it using the local storage. I built this
+                    application application with create-react-app,
+                    styled-components for CSS @reach/router for routing,
+                    prop-types for components' output checking, and font-awesome
+                    icons.
                   </p>
-                  <a
-                    href="https://unsplash-marinanicolai.vercel.app/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <div className="flex text-base sm:text-lg lg:text-base xl:text-lg font-medium transition-colors duration-200 focus:ring-2 focus:ring-offset-2 focus:ring-current focus:outline-none rounded-md text-orange-600 hover:text-orange-700 dark:text-orange-500 dark:hover:text-orange-400 mt-2">
-                      <p>Visit the live website</p>
-                    </div>
-                  </a>
-                  <a
-                    href="https://github.com/marinanicolai/unsplash-hooks-portofolio"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <div className="inline-flex text-base sm:text-lg lg:text-base xl:text-lg font-medium transition-colors duration-200 focus:ring-2 focus:ring-offset-2 focus:ring-current focus:outline-none rounded-md text-orange-600 hover:text-orange-700 dark:text-orange-500 dark:hover:text-orange-400 mt-2">
-                      <p>Code source</p>
-                    </div>
-                  </a>
+                  <div>
+                    <a
+                      href="https://unsplash-marinanicolai.vercel.app"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <div className="inline-flex text-base sm:text-lg lg:text-base xl:text-lg font-medium transition-colors duration-200 focus:ring-2 focus:ring-offset-2 focus:ring-current focus:outline-none rounded-md text-orange-600 hover:text-orange-700 dark:text-orange-500 dark:hover:text-orange-400 mt-2">
+                        <img className="tv" src={TvImage} alt="tvimg" />
+                        <p>Visit the site</p>
+                      </div>
+                    </a>
+                  </div>
+
+                  <div>
+                    <a
+                      href="https://github.com/marinanicolai/unsplash-hooks-portofolio"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <div className="inline-flex text-base sm:text-lg lg:text-base xl:text-lg font-medium transition-colors duration-200 focus:ring-2 focus:ring-offset-2 focus:ring-current focus:outline-none rounded-md text-orange-600 hover:text-orange-700 dark:text-orange-500 dark:hover:text-orange-400 mt-2">
+                        <img
+                          className="tv"
+                          src={GithubImage}
+                          alt="github-img"
+                        />
+                        <p>Code source</p>
+                      </div>
+                    </a>
+                  </div>
                 </div>
-                <div>
+                <div className="card highlight-on-hover">
                   <img
                     className="select-none"
                     src={FirstProjectImage}
                     alt="firstprojectimg"
                   />
+                  <p className="title">Photo app</p>
                 </div>
               </div>
             </li>
+            {/* second */}
             <li className="bg-coolGray-100 dark:bg-blueGray-800 rounded-3xl">
               <div className="p-6 md:p-10 flex flex-col-reverse ">
                 <div className="mt-8 md:max-w-lg">
@@ -72,36 +86,52 @@ export default function Toolbox() {
                     Next.js hotel App
                   </p>
                   <p className="text-coolGray-600 dark:text-coolGray-400 prose leading-6 mb-3">
-                    <ul>
-                      <li>The project was created using Next.js and React</li>
-                      <li> Incorporated use of chakra-ui library</li>
-                    </ul>
+                    This is a fun-based project that fetches data using
+                    React.js, allowing users to see and and search for specific
+                    rental property within database. Users can filter the
+                    properties based on purpose, rent frequency, price,
+                    size(sqft) and rooms. After selecting the item the user can
+                    access additional information about it. I built this
+                    application with create-react-app, using styled-components
+                    for CSS; Next.js for SEO purpose, and font-awesome icons.
                   </p>
-                  <a
-                    href="https://real-estate-tau-lemon.vercel.app/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <div className="flex text-base sm:text-lg lg:text-base xl:text-lg font-medium transition-colors duration-200 focus:ring-2 focus:ring-offset-2 focus:ring-current focus:outline-none rounded-md text-orange-600 hover:text-orange-700 dark:text-orange-500 dark:hover:text-orange-400 mt-2">
-                      <p>Visit the live website</p>
-                    </div>
-                  </a>
-                  <a
-                    href="https://github.com/marinanicolai/real-estate-next.js"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <div className="inline-flex text-base sm:text-lg lg:text-base xl:text-lg font-medium transition-colors duration-200 focus:ring-2 focus:ring-offset-2 focus:ring-current focus:outline-none rounded-md text-orange-600 hover:text-orange-700 dark:text-orange-500 dark:hover:text-orange-400 mt-2">
-                      <p>Code source</p>
-                    </div>
-                  </a>
+                  <div>
+                    <a
+                      href="https://real-estate-tau-lemon.vercel.app/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <div className="inline-flex text-base sm:text-lg lg:text-base xl:text-lg font-medium transition-colors duration-200 focus:ring-2 focus:ring-offset-2 focus:ring-current focus:outline-none rounded-md text-orange-600 hover:text-orange-700 dark:text-orange-500 dark:hover:text-orange-400 mt-2">
+                        <img className="tv" src={TvImage} alt="tvimg" />
+                        <p>Visit the site</p>
+                      </div>
+                    </a>
+                  </div>
+
+                  <div>
+                    <a
+                      href="https://real-estate-tau-lemon.vercel.app/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <div className="inline-flex text-base sm:text-lg lg:text-base xl:text-lg font-medium transition-colors duration-200 focus:ring-2 focus:ring-offset-2 focus:ring-current focus:outline-none rounded-md text-orange-600 hover:text-orange-700 dark:text-orange-500 dark:hover:text-orange-400 mt-2">
+                        <img
+                          className="tv"
+                          src={GithubImage}
+                          alt="github-img"
+                        />
+                        <p>Code source</p>
+                      </div>
+                    </a>
+                  </div>
                 </div>
-                <div>
+                <div className="card highlight-on-hover">
                   <img
                     className="select-none"
                     src={SecondProjectImage}
                     alt="secondprojectimg"
                   />
+                  <p className="title">Hotel app</p>
                 </div>
               </div>
             </li>
